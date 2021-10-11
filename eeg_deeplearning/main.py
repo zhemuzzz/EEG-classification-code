cb2d0f226821596580903b395fb3e9fc9942ebc1
zhemuzzz <67012986+zhemuzzz@users.noreply.github.com>
2021-10-11 11:02:08 +0800

    Update main.py

--- eeg_deeplearning/main.py
+++ eeg_deeplearning/main.py
@@ -23,6 +23,45 @@ def gpu_start():
         tf.config.experimental.set_memory_growth(physical_devices[i], True)
 
 
+#----------------------------------------------记录模型训练过程--------------------------------------         
+def plot_history(history, result_dir, fold_index):
+    plt.plot(history.history['accuracy'], marker='.')
+    plt.plot(history.history['val_accuracy'], marker='.')
+    plt.title('model accuracy')
+    plt.xlabel('epoch')
+    plt.ylabel('accuracy')
+    plt.grid()
+    plt.legend(['acc', 'val_acc'], loc='lower right')
+    plt.savefig(os.path.join(result_dir, 'model_accuracy-' + str(fold_index) + '.png'))
+    plt.close()
+
+    plt.plot(history.history['loss'], marker='.')
+    plt.plot(history.history['val_loss'], marker='.')
+    plt.title('model loss')
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.grid()
+    plt.legend(['loss', 'val_loss'], loc='upper right')
+    plt.savefig(os.path.join(result_dir, 'model_loss-' + str(fold_index) + '.png'))
+    plt.close()
+
+
+def save_history(history, result_dir, fold_index):
+    loss = history.history['loss']
+    acc = history.history['accuracy']
+    val_loss = history.history['val_loss']
+    val_acc = history.history['val_accuracy']
+    nb_epoch = len(acc)
+
+    # Todo:可以改成csv格式
+    with open(os.path.join(result_dir, 'train-result-' + str(fold_index) + '.txt'), 'w') as fp:
+        fp.write('epoch\tloss\tacc\tval_loss\tval_acc\n')
+        for i in range(nb_epoch):
+            fp.write('{}\t{}\t{}\t{}\t{}\n'.format(
+                i, loss[i], acc[i], val_loss[i], val_acc[i]))
+
+            
+#-----------------------------------------------训练EEG模型--------------------------------------     
 def train_eeg_model(params, train_dataset, valid_dataset, fold_index):
     chans, samples = params['chans'], params['samples']
     # multi gpu
@@ -132,7 +171,7 @@ def train_main(params, X_data, y_data):
     print('cross subjects all acc list: ', params['acc'])
     print('cross subjects mean acc: ', np.mean(params['acc']))
 
-    # ------------------------------ leave one subject out----------------
+    # ------------------------------ 留一交叉验证方式 leave one subject out-----------------------------------
     # params['mode_'] = 'leaveOneOut'
     # y_label = y_data[:, 1:4]
     # y_index = y_data[:, 0]
